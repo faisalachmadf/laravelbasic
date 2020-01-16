393c1eeabbfb84b269bd1ade6ea526fd495fd26a
faisalachmadf <faisalachmadf@gmail.com>
2020-01-16 16:27:07 +0700

    tes view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Percobaan Laravel
                 </div>
 
                 <div class="links">
